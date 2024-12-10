c181bde20d84739b21e5f0f5d0d531e2503314d2
mrdapoyo <wiiuforever25@gmail.com>
2024-12-10 21:03:37 +0100

    blog working!

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs-extra');
 const path = require('path');
 const ejs = require('ejs');
+const { read } = require('fs');
 
 // Directory paths
 const srcDir = path.join(__dirname, 'src');
@@ -14,19 +15,53 @@ if (!fs.existsSync(outputDir)) {
     fs.mkdirSync(path.join(outputDir, staticDir));
 }
 
+function readDir(dir) {
+    const files = fs.readdirSync(dir);
+    let pages = [];
+    files.forEach(file => {
+        const filePath = path.join(dir, file);
+        const stats = fs.statSync(filePath
+        );
+        if (stats.isDirectory()) {
+            pages = pages.concat(readDir(filePath));
+        } else if (file.endsWith('.ejs')) {
+            const data = fs.readFileSync(filePath, 'utf8');
+            pages.push({
+                name: file.replace('.ejs', ''),
+                cleanPath: filePath.replace(pagesDir, '').replace('.ejs', '.html').replace(/\\/g, '/'),
+                filename: file.replace('.ejs', '.html'),
+                path: filePath,
+                body: data,
+                creationDate: stats.birthtime,
+                lastModifiedDate: stats.mtime,
+                directory: false
+            });
+        }
+    });
+    return pages;
+}
+
+function renderPage(page) {
+    const templatePath = path.join(templatesDir, "base.ejs");
+    const outputPath = path.join(outputDir, page.cleanPath);
+    var arguments = { static: staticDir, body: null, title: page.name, description: "We make cool stuff!" };
+    arguments.body = ejs.render(page.body, arguments);
+    fs.ensureDirSync(path.dirname(outputPath));
+    ejs.renderFile(templatePath, arguments, (err, str) => {
+        if (err) {
+            console.error(`Error rendering ${templatePath}:`, err);
+            return;
+        }
+        if (!fs.existsSync(path.dirname(outputPath))) {
+            fs.mkdirSync(path.dirname(page.path), { recursive: true });
+        }
+        fs.writeFileSync(outputPath, str);
+        console.log(`\tCreated: ${page.cleanPath}`);
+    });
+}
+
 const pagesDir = path.join(srcDir, 'pages');
-const pages = fs.readdirSync(pagesDir).map(file => {
-    const filePath = path.join(pagesDir, file);
-    const data = fs.readFileSync(filePath, 'utf8');
-    const stats = fs.statSync(filePath);
-    return {
-        name: file.replace('.ejs', ''),
-        filename: file.replace('.ejs', '.html'),
-        body: data,
-        creationDate: stats.birthtime,
-        lastModifiedDate: stats.mtime
-    };
-});
+const pages = readDir(pagesDir);
 
 // Render pages
 if (pages.length === 0) {
@@ -35,19 +70,7 @@ if (pages.length === 0) {
 }
 
 pages.forEach(page => {
-    const templatePath = path.join(templatesDir, "base.ejs");
-    const outputPath = path.join(outputDir, page.filename);
-    var arguments = { static: staticDir, body: null, title: page.name, description: "We make cool stuff!" };
-    arguments.body = ejs.render(page.body, arguments);
-    ejs.renderFile(templatePath, arguments, (err, str) => {
-        if (err) {
-            console.error(`Error rendering ${templatePath}:`, err);
-            return;
-        }
-
-        fs.writeFileSync(outputPath, str);
-        console.log(`Generated ${outputPath}`);
-    });
+    renderPage(page);
 });
 
 // Copy static files
